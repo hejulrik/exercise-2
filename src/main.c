89fbbaf47833deb88204462aeb5c966892a48af8
Arnström Ulrik (66108429) <ulrik.s.arnstrom@forsakringskassan.se>
2014-04-14 13:41:01 +0200

    Ulrik skriver och testar git

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int fib(unsigned n) {
-    return n < 2 ? n : fib(n - 1) + fib(n - 2);
+    return n < 2 ? n : fib(n - 1) + fib(n - 2) ;
 }
 
 int main() {
     printf("%d\n", fib(10));
+    printf("%d\n", fib(11));
 }
